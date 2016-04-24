6edf4f721ab617062d2eb967087ab67dbee40e3b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-04-23 19:53:36 -0700

    inline stuff

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-var $ = require('jquery');
-var d3 = require('d3');
-var barchart = require('./barchart');
+const $ = require('jquery');
+const d3 = require('d3');
   
+// Load the data
 d3.csv('data.csv', function(err, data) {
   if (err) throw err;
 
-  var majors = {}
+  let majors = {}
   data.forEach(function(d) {
     if (majors[d.Major]) {
       majors[d.Major] ++;
@@ -14,18 +14,42 @@ d3.csv('data.csv', function(err, data) {
     }
   })
 
-  var majorNames = Object.keys(majors);
-  var majorData = [];
+  let majorNames = Object.keys(majors);
+  let majorData = [];
 
   majorNames.forEach(function(name) {
     majorData.push({ value: majors[name], name: name })
   })
 
-  var alphaSortedMajors = majorData.sort((a, b) => a.name.localeCompare(b.name))
+  let alphaSortedMajors = majorData.sort((a, b) => a.name.localeCompare(b.name))
 
-  var questions = [{
+  let questions = [{
     question: "1. Frequency/counting: How many students are in each major (in this class)?",
-    render: (container) => barchart.render(container, { data: alphaSortedMajors })
+    explanation: `
+    This graphic allows the user to determine how many students are in each major.
+    It does this well given the fact that it clearly labels the number of students beside the major.
+    Also, by using color and bar size it allows the viewer to determine which majors are more or less popular.
+    Finally, by sorting in alphabetical order, the viewer can quickly scan to the major of interest.
+    `,
+    render: (container) => {
+      let data = alphaSortedMajors;
+      let values = data.map(d => d.value)
+      let max = d3.max(values)
+      let scale = d3.scale.linear().domain([0, max])
+      let bgColor = d3.scale.linear().domain([0, max]).range(['#FF393E', '#74FF63'])
+      let txtColor = d3.scale.linear().domain([0, max]).range(['#023100', '#360000'])
+
+      d3.select(container)
+      .selectAll("div")
+      .data(data)
+      .enter().append("div")
+      .style("width", d => `${scale(d.value) * 100}%`)
+      .style("background-color", d => bgColor(d.value))
+      .style("color", d => txtColor(d.value))
+      .style("margin", '1px')
+      .style('padding', '5px')
+      .text(d => `${d.name} (${d.value})`);
+    }
   },{
     question: "2. Did major have any affect on the score? Which majors scored higher/lower?",
     render: function() {
@@ -43,14 +67,21 @@ d3.csv('data.csv', function(err, data) {
     }
   }]
 
+  // Render everything!
   questions.forEach(function(q) {
-    var container = $('<div>').css({
+    let container = $('<div>').css({
       'font-family': 'helvetica',
       'margin': '20px',
-    }).text(q.question)
-    var chart = $('<div>')
+    }).append($('<h2>').text(q.question))
+    let chart = $('<div>')
     container.append(chart)
-    if (q.render) q.render(chart.get(0))
+    if (q.render) {
+      q.render(chart.get(0))
+      if (q.explanation)
+        chart.after($('<p>').text(q.explanation).css({
+          'font-size': '1.2em'
+        }))
+    }
     $('body').append(container);
   })
 });
