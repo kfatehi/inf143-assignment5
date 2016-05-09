198bb4e22d1abf02767c2361aa580b22220422bb
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-05-09 15:08:10 -0700

    treemap labels

--- src/treemap.js
+++ src/treemap.js
@@ -4,7 +4,7 @@ export default (h, w) => (container, data) => {
     color = d3.scale.category20c();
 
   var treemap = d3.layout.treemap()
-    .padding(4)
+    .padding([20,4,4,4])
     .size([width, height])
     .value(function(d) { return d.size; });
 
@@ -17,18 +17,17 @@ export default (h, w) => (container, data) => {
   var cell = svg.data(data).selectAll("g")
     .data(treemap.nodes)
     .enter().append("g")
-    .attr("class", "cell")
-    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
 
   cell.append("rect")
     .attr("width", function(d) { return d.dx; })
     .attr("height", function(d) { return d.dy; })
-    .style("fill", function(d) { return d.children ? color(d.name) : null; });
+    .style("fill", d => d.children ? color(d.name) : null)
 
   cell.append("text")
-    .attr("x", function(d) { return d.dx / 2; })
-    .attr("y", function(d) { return d.dy / 2; })
+    .attr("x", d => d.dx / 2)
+    .attr("y", d => d.children ? 10 : d.dy / 2)
     .attr("dy", ".35em")
     .attr("text-anchor", "middle")
-    .text(function(d) { return d.children ? null : d.size; });
+    .text(d => d.children ? d.name : d.size)
 }
