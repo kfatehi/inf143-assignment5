fe04eeac1c0a45bb361f2bceebcdb9cd256072df
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-05-31 20:52:52 -0700

    scatter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 const $ = require('jquery');
 const d3 = require('d3');
-import linechart from './linechart';
+import plot from './scatterplot';
 
 const sourceFiles = [
-  'index.html',
-  'style.css',
-  'src/sunburst.js',
-  'src/treemap.js',
   'src/index.js',
+  'src/scatterplot.js',
 ]
 const dataFiles = [{
   title: 'Red Wine',
@@ -42,12 +39,17 @@ $(() => {
       $('body').append(cols.slice(1, cols.length-1).map(function(name, idx) {
         return $('<button>').text(name).click(function() {
 
-          let data = dataRows.map(row => [row[idx], row[outputIdx]]);
+          let data = dataRows.map(row => {
+            return {
+              quality: row[outputIdx],
+              metric: row[idx], 
+            }
+          }).sort(function(a,b) {
+            return a.metric - b.metric;
+          });
 
-          let svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
           container.empty();
-          container.append(svg);
-          linechart(svg, data);
+          plot(container.get(0), data);
         });
       }));
       $('body').append(container);
@@ -55,7 +57,11 @@ $(() => {
   });
 
   setTimeout(function() {
-    // Render remarks
+    function remarks() {
+      return `
+      <p>I chose to create a *name of vis* and this is the *brief explanation*.</p>
+    `
+    }
     $('body').append($('<div>').html(remarks()));
 
     // Render the source files
@@ -64,75 +70,3 @@ $(() => {
 
 })
 
-const prep = (rows) => {
-  return [{
-    name: '',
-    children: rows.map(row => {
-      return {
-        name: row[0],
-        children: row.slice(1, row.length-1)
-          .filter(i => i.length > 0)
-          .map(i=>{
-            return {
-              name: 'Anonymous Faculty',
-              size: +i
-            }
-          })
-      }
-    })
-  }]
-}
-
-let h = 1000;
-let w = 1000;
-
-let charts = [{
-  title: df => `${df.title} Sunburst`,
-  render: function(container, data) {
-    let svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
-    $(container).append($(svg));
-    sunburst(h,w)(svg, data);
-  },
-  explanation: `
-  <p>This visualization (sunburst) allows the viewer to quickly pick out
-  which schools are the most high impact relative to each other as
-  well as pick out which faculty are highest impact relative to each
-  other. Color is used to distinguish one school from another. The
-  same color is used for each faculty member of a school in order
-  to make it easy to keep in mind which school's faculty is being
-  viewed.</p>
-
-  <p>I chose this visualization because I feel that the data is in
-  fact heirarchical in nature and given that the viewer wants to
-  view the whole as well as the sum of its parts 
-  (the school as the faculty members), the sunburst was appropriate.</p>
-  `,
-  },{
-  title: df => `${df.title} Treemap`,
-    render: function(container, data) {
-      let svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
-      $(container).append($(svg));
-      treemap(h,w)(svg, data)
-    },
-    explanation: `
-    <p>This visualization (treemap), similar to the sunburst, works
-    well with this type of nested data. The choice of colors are
-    also simply for separating out the schools, which make up the
-    main blocks (first level) of the tree map. The faculty members
-    make up the second level, with their size a function of their
-    individual impact. Naturally, the size of the parent (the school)
-    is a function of all the individual impacts of the faculty.</p>
-
-      <p>I chose this visualization because it was another way to view
-    heirarchical data. In the treemap we can still view the whole
-    as well as the sum of its parts. I was also curious how different
-    the treemap and the sunburst would be in reprenting exactly
-    the same data and structure.</p>
-    `,
-}]
-
-function remarks() {
-  return `
-  <p>I chose to create a *name of vis* and this is the *brief explanation*.</p>
-  `
-}
